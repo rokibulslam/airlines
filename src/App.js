9dd218e148d240fdd35456e280dceae9c55f2a5e
rokibulslam <rokibulslam6@gmail.com>
2023-01-28 12:17:45 +0600

    Project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Result from './Pages/Result/Result';
+import Search from './Pages/Search/Search';
+
+function App() {
+  return (
+    <div className="App">
+      <Result />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
