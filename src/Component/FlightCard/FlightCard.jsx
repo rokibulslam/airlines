8d55cdf9e91298b2884e7a6ebc0e3fbe86ce8b75
rokibulslam <rokibulslam6@gmail.com>
2023-01-29 09:05:31 +0600

    flight card

--- src/Component/FlightCard/FlightCard.jsx
+++ src/Component/FlightCard/FlightCard.jsx
@@ -2,88 +2,83 @@ import React from "react";
 import { FaPlane } from "react-icons/fa";
 import { RiLuggageCartFill } from "react-icons/ri";
 import { MdOutlineAirlineSeatReclineNormal } from "react-icons/md";
+import {AiOutlineArrowRight} from "react-icons/ai"
 const FlightCard = () => {
   return (
     <div className="custom-shadow-sm">
-      <div className="flex flex-col lg:flex-row flex-wrap justify-between space-y-2 p-3">
-        {/* Time  */}
+      <p className="text-purple-800 font-bold px-2 text-center">
+        US-Bangla Airlines |
+        <span className="tex  text-sm text-black font-light">BS 145</span>{" "}
+      </p>
+      <div className="flex flex-col lg:flex-row p-3 lg:space-x-10 justify-center">
+        {/* FROM TO  */}
         <div className="flex justify-between lg:space-x-10">
-          {/* Airlines Name */}
+          {/* From  */}
           <div>
-            <p className="text-purple-800 font-bold">US-Bangla Airlines</p>
-            <p className="tex  text-sm">BS 145</p>
-            <p className=" text-sm">
-              1H 5Min |{" "}
-              <span className="text-purple-800 text-sm">Non Stops</span>
-            </p>
+            <p className="font-bold">DAC - 11:15</p>
+            <p className="">Dhaka, BD</p>
+            <p className="text-gray-500 text-sm">Sun Jan 2023</p>
           </div>
-          {/* Duration */}
+          {/* {Duration} */}
           <div className="text-center">
             <p className="text-sm">1H 5Min</p>
             <div className="flex items-center">
-              <div className="h-4 w-4 bg-slate-500 rounded-full"></div>
-              <div className="h-1 w-32 bg-slate-500"></div>
-              <p className="text-2xl">
+              <div className="h-3 w-3 bg-slate-500 rounded-full"></div>
+              <div className="h-[2px] w-24 bg-slate-500"></div>
+              <p className="text-2xl text-purple-800">
                 <FaPlane />
               </p>
-              <div className="h-4 w-4 bg-slate-500 rounded-full"></div>
+              <div className="h-3 w-3 bg-slate-500 rounded-full"></div>
             </div>
-          </div>
-        </div>
-        <div className="flex justify-between space-x-10">
-          {/* From  */}
-          <div>
-            <p>DAC - 11:15</p>
-            <p className=" font-bold">Dhaka, BD</p>
-            <p className="text-gray-500 text-sm">Sun Jan 2023</p>
+            <p>
+              <span className="text-black text-sm text-center">Non Stops</span>
+            </p>
           </div>
           {/* To  */}
-          <div className="text-end">
+          <div className="font-bold lg:text-start text-end">
             <p>DAC - 11:15</p>
-            <p className="font-bold">Dhaka, BD</p>
+            <p className="">Dhaka, BD</p>
             <p className="text-gray-500 text-sm">Sun Jan 2023</p>
           </div>
         </div>
-        {/* Airlines */}
-
-        {/* Reservation */}
-
-        {/* Book Now */}
-        <div className="flex justify-between items-center lg:space-x-10">
-          <div className="space-y-2">
-            {/* Econnomy */}
-            <div>
-              <p>Economy</p>
-              <div className="flex items-center">
-                <p className="text-xl text-purple-800">
-                  <RiLuggageCartFill />
-                </p>
-                <p className="text-sm">20 Kg</p>
-              </div>
+        {/* OTHERS  */}
+        <div className="flex justify-between items-start lg:space-x-10 space-x-3">
+          {/* Econnomy */}
+          <div>
+            <p>Economy</p>
+            <div className="flex items-center">
+              <p className="text-xl text-purple-800">
+                <RiLuggageCartFill />
+              </p>
+              <p className="">20 Kg</p>
             </div>
-            {/* Refund */}
-            <div>
-              <p className="">Refundable</p>
-              <div className="flex items-center">
-                <p className="text-xl text-purple-800">
-                  <MdOutlineAirlineSeatReclineNormal />
-                </p>
-                <p className="text-sm">1 Seat</p>
-              </div>
-            </div>
-            {/* agent */}
           </div>
+          {/* Refund */}
+          <div>
+            <p className="">Refundable</p>
+            <div className="flex items-center">
+              <p className="text-xl text-purple-800">
+                <MdOutlineAirlineSeatReclineNormal />
+              </p>
+              <p className="text-sm">1 Seat</p>
+            </div>
+          </div>
+          {/* agent */}
           <div className="text-start">
-            <p className="font-bold ">Agent Fare 10,133</p>
+            <p className="font-bold ">Customer Fare 10,133</p>
             <p className="text-purple-800 text-sm">Client Fare 11,535</p>
             <p className="text-purple-800 text-sm">Commission 11,535</p>
           </div>
         </div>
-        <div className="flex justify-between lg:items-center lg:justify-center lg:space-y-3 lg:flex-col">
-          <p className="font-bold text-sm text-purple-800">Flight Details</p>
-          <button className="px-3 py-2 rounded-md bg-black text-gray-100 hover:bg-red-600 ease-in duration-300 hover:shadow-lg custom-btn text-lg">
-            Book Now
-          </button>
+        {/* Button  */}
+        <div className="flex justify-between lg:justify-center lg:space-y-3 lg:flex-col bg-gray-400 items-center rounded-r-md lg:bg-inherit">
+          <p className="font-bold text-sm text-black px-2">Flight Details</p>
+          <div className="flex items-center gap-x-2 px-3 py-1 rounded-md bg-purple-800 text-gray-100 hover:bg-red-600 ease-in duration-300 hover:shadow-lg">
+            <button>Book Now</button>
+            <span>
+              <AiOutlineArrowRight />
+            </span>
+          </div>
         </div>
       </div>
     </div>
